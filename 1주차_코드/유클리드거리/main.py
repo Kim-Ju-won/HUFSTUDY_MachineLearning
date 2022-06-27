a8e6dd4255ca5f451d75696a4121aa0922219579
김주원 <kjwt1124@icloud.com>
2022-06-27 19:48:12 +0900

    Upload Euclid distance algorithm 22 JUN 27

---
+++ 1주차_코드/유클리드거리/main.py
@@ -0,0 +1,15 @@
+import numpy as np
+from math import sqrt
+
+
+def distance(user_1, user_2):
+    """유클리드 거리를 계산해주는 함수"""
+    # 코드를 쓰세요 
+    
+
+# 실행 코드
+user_1 = np.array([0, 1, 2, 3, 4, 5])
+user_2 = np.array([0, 1, 4, 6, 1, 4])
+
+distance(user_1, user_2)
+
