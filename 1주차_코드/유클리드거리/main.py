f7891de2e834552d1654551bb3deab019dba50b1
김주원 <kjwt1124@icloud.com>
2022-07-04 17:16:42 +0900

    Upload week 1 source code

--- 1주차_코드/유클리드거리/main.py
+++ 1주차_코드/유클리드거리/main.py
@@ -4,6 +4,10 @@ from math import sqrt
 
 def distance(user_1, user_2):
     """유클리드 거리를 계산해주는 함수"""
+    result = user_1 - user_2
+    result = result **2
+    ans = sqrt(np.sum(result))
+    return ans
     # 코드를 쓰세요 
     
 
